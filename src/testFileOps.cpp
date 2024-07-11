6dba5eccd18680687b199a54bc6d70d8fbc4aca7
Canneph <kenneth08202004@gmail.com>
2024-07-11 12:55:32 -0700

    testFileOps file compiles (unit test 1)
    - testFileOps currently is a clone of main.cpp

---
+++ src/testFileOps.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "UserInterface.h"
+#include "SystemController.h"
+
+// Main Function
+// ============================================
+// Function: main
+int main() {
+    start();
+    activateUI();
+    return 0;
+}
