5cde7678a95a3c5b5d04b65dccf3db277cdddd78
Canneph <kenneth08202004@gmail.com>
2024-07-14 14:04:30 -0700

    userInterface has robust input checking now

--- include/UserInterface.h
+++ include/UserInterface.h
@@ -8,5 +8,10 @@ void start();
 int readIntegerInput(int min, int max);
 void activateUI();
 void shutdown();
+void displayMainMenu();
+void displayProductMaintenance();
+void displayChangeRequestMaintenance();
+void displayChangeItemMaintenance();
+void displayReportGeneration();
 
 #endif // USERINTERFACE_H
--- src/UserInterface.cpp
+++ src/UserInterface.cpp
@@ -30,17 +30,21 @@ void start();
 
 // ---------------------------------------------------------
 // Function to read an integer input within a specified range
-int readIntegerInput(int min, int max) {
+int readIntegerInput(MenuFuncPtr func, int min, int max) {
     int choice;
 
     // Prompt the user
+	system("clear");
+	func();
     std::cout << "Enter the number you want [" << min << "-" << max << "]: ";
 
     // Read the integer input and validate
     while (!(std::cin >> choice) || choice < min || choice > max) {
         std::cin.clear(); // Clear the error flag
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore invalid input
-        std::cout << "Invalid input. Please enter a number between " << min << " and " << max << ".\n";
+		system("clear");
+		std::cout << "Invalid input. Please enter a number between " << min << " and " << max << ".\n";
+		func();
         std::cout << "Enter the number you want [" << min << "-" << max << "]: ";
     }
 
@@ -50,52 +54,63 @@ int readIntegerInput(int min, int max) {
     return choice;
 }
 
+void displayMainMenu() {
+	std::cout << "\n====== Main Menu ======\n";
+	std::cout << "1) Product Maintenance\n";
+	std::cout << "2) Change Request Maintenance\n";
+	std::cout << "3) Change Item Maintenance\n";
+	std::cout << "4) Report Generation\n";
+	std::cout << "0) Exit\n";
+}
+
+void displayProductMaintenance() {
+	std::cout << "\n====== Product Maintenance ======\n";
+	std::cout << "1) Create a Product\n";
+	std::cout << "2) Create a Release of a Product\n";
+	std::cout << "0) Return to Main Menu\n";
+}
+
+void displayChangeRequestMaintenance() {
+	std::cout << "\n====== Change Request Maintenance ======\n";
+	std::cout << "1) Add a new change request\n";
+	std::cout << "0) Go back to Main Menu\n";
+}
+
+void displayChangeItemMaintenance() {
+	std::cout << "\n====== Change Item Maintenance ======\n";
+	std::cout << "1) Query Change Item to Screen\n";
+	std::cout << "2) Update/Assess Change Item\n";
+	std::cout << "0) Return to Main Menu\n";
+}
+
+void displayReportGeneration() {
+	std::cout << "\n====== Report Generation ======\n";
+	std::cout << "1) Generate Report #1\n";
+	std::cout << "2) Generate Report #2\n";
+	std::cout << "0) Return to Main Menu\n";
+}
+
+
+
 // ---------------------------------------------------------
 // Function: activateUI
 void activateUI() {
     int choice = -1;
     do {
-      //   system("clear");
-        std::cout << "\n====== Main Menu ======\n";
-        std::cout << "1) Product Maintenance\n";
-        std::cout << "2) Change Request Maintenance\n";
-        std::cout << "3) Change Item Maintenance\n";
-        std::cout << "4) Report Generation\n";
-        std::cout << "0) Exit\n";
-        choice = readIntegerInput(0,4);
-
+        choice = readIntegerInput(displayMainMenu,0,4);
         switch (choice) {
             case 1: 
-            //     system("clear");
-                std::cout << "\n====== Product Maintenance ======\n";
-                std::cout << "1) Create a Product\n";
-                std::cout << "2) Create a Release of a Product\n";
-                std::cout << "0) Return to Main Menu\n";
-                choice = readIntegerInput(0,2);
+                choice = readIntegerInput(displayProductMaintenance,0,2);
                 handleProductMaintenance(choice); break;
         
             case 2: 
-            //     system("clear");
-                std::cout << "\n====== Change Request Maintenance ======\n";
-                std::cout << "1) Add a new change request\n";
-                std::cout << "0) Go back to Main Menu\n";
-				choice = readIntegerInput(0,1);
+				choice = readIntegerInput(displayChangeRequestMaintenance,0,1);
                 handleChangeRequestMaintenance(choice); break;
             case 3: 
-            //     system("clear");
-                std::cout << "\n====== Change Item Maintenance ======\n";
-                std::cout << "1) Query Change Item to Screen\n";
-                std::cout << "2) Update/Assess Change Item\n";
-                std::cout << "0) Return to Main Menu\n";
-				choice = readIntegerInput(0,2);
+				choice = readIntegerInput(displayChangeItemMaintenance,0,2);
                 handleChangeItemMaintenance(choice); break;
             case 4: 
-            //     system("clear");
-                std::cout << "\n====== Report Generation ======\n";
-                std::cout << "1) Generate Report #1\n";
-                std::cout << "2) Generate Report #2\n";
-                std::cout << "0) Return to Main Menu\n";
-				choice = readIntegerInput(0,2);
+				choice = readIntegerInput(displayReportGeneration,0,2);
                 handleReportGeneration(choice); break;
             case 0: 
                 shutdown();
