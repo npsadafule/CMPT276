0e98fa4dacac2f02736e4c0482985fed93c828e0
RownakDiganta <145170131+RownakDiganta@users.noreply.github.com>
2024-07-16 21:08:47 -0700

    Update UserInterface.h

--- include/UserInterface.h
+++ include/UserInterface.h
@@ -7,14 +7,36 @@ typedef void (*MenuFuncPtr)();
 // ============================================
 // Function Declarations
 // ============================================
+
+// Starts the user interface
 void start();
+
+// Reads an integer input from the user
+// Parameter: func (The function to call for displaying the menu)
+// Parameter: min (The minimum acceptable value)
+// Parameter: max (The maximum acceptable value)
+// Returns: int (The user's input within the specified range)
 int readIntegerInput(MenuFuncPtr func, int min, int max);
+
+// Activates the user interface
 void activateUI();
+
+// Shuts down the user interface
 void shutdown();
+
+// Displays the main menu
 void displayMainMenu();
+
+// Displays the product maintenance menu
 void displayProductMaintenance();
+
+// Displays the change request maintenance menu
 void displayChangeRequestMaintenance();
+
+// Displays the change item maintenance menu
 void displayChangeItemMaintenance();
+
+// Displays the report generation menu
 void displayReportGeneration();
 
 #endif // USERINTERFACE_H
