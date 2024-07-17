b088667a2e391c0960aa7a28e08b276d16d56a67
RownakDiganta <145170131+RownakDiganta@users.noreply.github.com>
2024-07-16 20:35:10 -0700

    Update ChangeRequest.h

--- include/ChangeRequest.h
+++ include/ChangeRequest.h
@@ -24,13 +24,37 @@ struct ChangeRequest {
 // ============================================
 // Function Declarations
 // ============================================
+
+// Opens the ChangeRequest file
 void openChangeRequestFile();
+
+
+// Closes the ChangeRequest file
 void closeChangeRequestFile();
+
+// Writes a ChangeRequest to the file
+// Parameter: changeRequest (The ChangeRequest to write)
 void writeChangeRequest(const ChangeRequest& changeRequest);
+
+// Seeks to the beginning of the ChangeRequest file
 void seekToBeginningOfChangeRequestFile();
 // bool getNextChangeRequest(ChangeRequest& changeRequest);
 // No display functions (display functions would go here based on other modules).
+
+// Retrieves a ChangeRequest by its primary key
+// Parameter: filename (The name of the file to retrieve the ChangeRequest from)
+// Parameter: reqName (The primary key of the requester)
+// Parameter: changeID (The primary key of the ChangeItem)
+// Parameter: changeRequest (The ChangeRequest object to store the retrieved data)
+// Returns: bool (true if retrieval was successful, false otherwise)
 bool retrieveChangeRequestByKey(const char* filename, const char* reqName, const int changeID, ChangeRequest& changeRequest);
+
+// Creates a new ChangeRequest
+// Parameter: requesterName (The name of the requester)
+// Parameter: changeID (The ID of the ChangeItem)
+// Parameter: reportedRelease (The reported release ID)
+// Parameter: reportedDate (The date the request was reported)
+// Parameter: priority (The priority of the request)
 void createChangeRequest(const char* requesterName,
 						 const int changeID,
 						 const char* reportedRelease,
@@ -38,7 +62,15 @@ void createChangeRequest(const char* requesterName,
 						 const char* priority);
 
 // For case scenarios NOT DONE
+// Queries a ChangeRequest
+// Parameter: productName (The name of the product)
+// Parameter: changeID (The ID of the ChangeItem)
 void queryChangeRequest(const std::string& productName, const std::string& changeID);
+
+// Updates the state of a ChangeRequest
+// Parameter: productName (The name of the product)
+// Parameter: changeID (The ID of the ChangeItem)
+// Parameter: newState (The new state of the ChangeRequest)
 void updateChangeRequest(const std::string& productName, const std::string& changeID, const std::string& newState);
 
 #endif // CHANGEREQUEST_H
