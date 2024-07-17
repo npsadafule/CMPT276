cbaedf4cfa640d195fa8737f63ee316ec416d8c1
RownakDiganta <145170131+RownakDiganta@users.noreply.github.com>
2024-07-16 20:47:21 -0700

    Update Product.h

--- include/Product.h
+++ include/Product.h
@@ -27,24 +27,69 @@ struct ProductRelease {
 // ============================================
 // Function Declarations: Product
 // ============================================
+
+
+// Opens the Product file
 void openProductFile();
+
+// Closes the Product file
 void closeProductFile();
+
+// Writes a Product to the file
+// Parameter: product (The Product to write)
 void writeProduct(const Product& product);
+
+// Seeks to the beginning of the Product file
 void seekToBeginningOfProductFile();
+
+
+// Displays a Product
+// Parameter: product (The Product to display)
 void displayProduct(const Product& product);
+
+
+// Displays the first 20 or fewer Products in the file
+// Parameter: filename (The name of the file to display Products from)
 void productFileDisplay20OrLess(const char* filename);
+
+// Retrieves a Product by its name
+// Parameter: filename (The name of the file to retrieve the Product from)
+// Parameter: productName (The name of the Product)
+// Parameter: product (The Product object to store the retrieved data)
+// Returns: bool (true if retrieval was successful, false otherwise)
 bool retrieveProductByName(const char* filename, const char* productName, Product& product);
+
+
+// Creates a new Product
+// Parameter: namePtr (The name of the Product)
 void createProduct(const char* namePtr); 
 
 // ============================================
 // Function Declarations: Product Release
 // ============================================
+
+
+// Opens the ProductRelease file
 void openProductReleaseFile();
+
+// Closes the ProductRelease file
 void closeProductReleaseFile();
+
+// Writes a ProductRelease to the file
+// Parameter: productRelease (The ProductRelease to write)
 void writeProductRelease(const ProductRelease& productRelease);
+
+// Seeks to the beginning of the ProductRelease file
 void seekToBeginningOfProductReleaseFile();
+
+// Displays a ProductRelease
+// Parameter: productRelease (The ProductRelease to display)
 void displayProductRelease(const ProductRelease& productRelease);
+
+// Displays the first 20 or fewer ProductReleases in the file
+// Parameter: filename (The name of the file to display ProductReleases from)
 void productReleaseFileDisplay20OrLess(const char* filename);
+
 bool retrieveProductReleaseByKey(const char* filename, const char* productReleaseName, const char* releaseID, ProductRelease& productRelease);
 void createProductRelease(const char* productName, const char* releaseID, const char* releaseDate);
 bool determineReleaseIDExistence(const char* releaseID);
